7b23f386bc47a4beb2c6deb4ccb3b231beaee7b1
Steve Melia <steve.j.melia@gmail.com>
2019-04-17 17:00:40 +0100

    Update to set gnome terminal font

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,14 @@ ln -s .dotfiles/.vim
 ln -s .dotfiles/.vimrc
 ln -s .dotfiles/.screenrc
 ln -s .dotfiles/.hgrc
+
+mkdir .fonts
+wget -nc https://github.com/powerline/fonts/blob/master/Inconsolata/Inconsolata%20Bold%20for%20Powerline.ttf?raw=true -O ~/.fonts/Inconsolata\ Bold\ for\ Powerline.ttf
+wget -nc https://github.com/powerline/fonts/blob/master/Inconsolata/Inconsolata%20for%20Powerline.otf?raw=true -O ~/.fonts/Inconsolata\ for\ Powerline.otf
+
+fc-cache -f -v
+
+# Writes default profile
+gnome_profile=$(dconf list /org/gnome/terminal/legacy/profiles:/)
+echo $gnome_profile
+dconf write /org/gnome/terminal/legacy/profiles:/${gnome_profile}font "'Inconsolata for Powerline Medium 12'"
